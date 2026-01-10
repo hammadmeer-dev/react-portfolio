532302612d9481a0fbcb8b01d6457e531389f1b7
hammad-wpbrigade <hammadfarooq@wpbrigade.com>
2026-01-11 02:19:02 +0500

    fix

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,6 +1,6 @@
 export default function HeaderLogo({ logo }) {
   return (
-    <span className="w-10 inline-block animate-jump">
+    <span className="w-10 inline-block">
       <img src={logo} alt="Logo" className="w-full h-auto" />
     </span>
   );
